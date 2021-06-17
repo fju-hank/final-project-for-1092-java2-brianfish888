da3dbcbfd9cd607500421c772ab2b50624e6706e
brian <brian830226@gmail.com>
2021-06-17 16:37:44 +0800

    package

---
+++ src/main/java/com/fju/Package.java
@@ -0,0 +1,13 @@
+package com.fju;
+
+public class Package {
+    String id;
+    int price;
+
+    public Package(String id,int price){
+            this.id= id;
+            this.price= price;
+
+    }
+
+}
--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -1,7 +1,37 @@
 package com.fju;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Hello");
+            while (true){
+                System.out.println("Please choose your option");
+                Scanner scanner=new Scanner(System.in);
+                String enter=scanner.next();
+                if(enter.startsWith("p")){
+                    Map<String , Package>packages = new HashMap<>();
+
+                    Package p1=new Package("252",28);
+                    Package p2=new Package("598",50);
+                    Package p3=new Package("388",74);
+
+                    packages.put(p1.id, p1);
+                    packages.put(p2.id, p2);
+                    packages.put(p3.id, p3);
+
+                    System.out.println("Enter your id:");
+                    Scanner a=new Scanner(System.in);
+                    String id=scanner.next();
+                    Package pack=packages.get(id);
+                    if(pack==null){
+                        System.out.println("Your package have not delivered or you enter wrong id");
+                    }else{
+                        System.out.println("ID:" + id + " , price :" + pack.price);
+                    }
+                    
+                }
+            }
     }
 }
